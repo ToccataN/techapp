b53e2bc4f4bd209d95fef02efb0f404712f23395
ToccataN <charlestonshadowsx@gmail.com>
2017-10-25 15:03:41 -0400

    push

--- app/controllers/new_controller.rb
+++ app/controllers/new_controller.rb
@@ -14,13 +14,7 @@ def getter
       @arr.push(@phrase[:phrase])
 
     else
-      @arr.each_with_index do |i, e|
-        if e < 9
-          @phrase = Phrasetable.find(e+1)
-        else
-          flash[:danger]= "Need to clear first!"
-        end
-      end
+      flash[:danger] = "try again, please"
     end
 
 
